650d0b26eec8943fba10a0933cce4a8eba805732
Ch4nYH <hfercxx@hotmail.com>
2020-05-19 12:30:07 +0800

    update

---
+++ src/components/Customer.vue
@@ -0,0 +1,39 @@
+<template>
+    <div style="position: absolute; top: 100px; background-color: white;left:0; right: 0; min-height: 500px;padding-top: 20px;">
+        <b-container>
+            <b-row>
+                <b-col>
+                    <Panel id="1"></Panel>
+                </b-col>
+                <b-col>
+                    <Panel id="2"></Panel>
+                </b-col>
+            </b-row>
+            <b-row style="padding-top: 20px;">
+                <b-col>
+                    <Panel id="3"></Panel>
+                </b-col>
+                <b-col>
+                    <Panel id="4"></Panel>
+                </b-col>
+            </b-row>
+            <b-row style="padding-top: 20px;">
+                <b-col>
+                    <Panel id="5"></Panel>
+                </b-col>
+                <b-col>
+                    <Panel id="6"></Panel>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+
+<script>
+    import Panel from './Panel'
+    export default {
+        components: {
+            Panel
+        }
+    }
+</script>
---
+++ src/components/Panel.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="panel-container" @click="showModal">
+        <b-img blank blank-color="#EEEEFF" width="175%" alt="placeholder" style="margin-top: 20px;"></b-img>
+        <p style="text-align: left; margin-left: 15%;margin-top: 10px;">Name</p>
+        <p style="text-align: right; margin-right: 15%;margin-top: 10px; font-size: 24px;">$99</p>
+        <div>
+        <b-modal :ref="'modal' + id" id="modal" title="BootstrapVue">
+            <p class="my-4">Hello from modal!</p>
+        </b-modal>
+        </div>
+    </div>
+</template>
+
+<style>
+.panel-container {
+    height: 300px;
+    box-shadow: 0 0 5px #ccc;
+    border-radius: 20px;
+}
+</style>
+
+<script>
+export default {
+    props: ['url', "id"],
+    methods: {
+        showModal () {
+            this.$refs['modal' + this.id].show()
+        }
+    }
+}
+</script>
